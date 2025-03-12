6a4613b43ba7cc059288b1408f0f4f362504c7bb
Pedro Tavares <1180582@isep.ipp.pt>
2025-03-12 02:02:34 +0000

    Implemented ApplicationDbContext, models, migrations, and updated database

---
+++ RotatingBearingAPI/Models/TestSequence.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RotatingBearingAPI.Models
+{
+    public class TestSequence
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public List<TestStep> Steps { get; set; } = new List<TestStep>();
+    }
+}
